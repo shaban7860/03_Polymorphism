4cfacc085eb8a23e9be08ceb4c0020111b6dc1a0
shaban7860 <113911526+shaban7860@users.noreply.github.com>
2022-09-21 16:51:50 +0530

    Add files via upload

---
+++ PolymorphismMain.java
@@ -0,0 +1,13 @@
+package com.mindgate.main;
+
+import com.mindgate.pojo.A;
+import com.mindgate.pojo.B;
+import com.mindgate.pojo.C;
+
+public class PolymorphismMain {
+	public static void main(String[] args) {
+		A a = new C();
+		a.show();
+	}
+
+}
